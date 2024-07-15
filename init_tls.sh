a3576a11e0c733bd599f0770c2b1daff8b4853fe
JaeJunday <wowns8114@gmail.com>
2024-07-15 21:59:50 +0900

    fix tls 자동생성 오류 수정

--- init_tls.sh
+++ init_tls.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "\n\n\n\n\n\n\n" | openssl req -x509 -days 365 -nodes -newkey rsa:2048 -keyout ./nginx/ssl/nginx.pem -out ./nginx/ssl/nginx.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./nginx/ssl/cert.key -out ./nginx/ssl/cert.pem -subj "/C=KR/ST=Seoul/L=Seoul/O=MyCompany/OU=MyDepartment/CN=www.mywebsite.com/emailAddress=admin@mywebsite.com"
 
 
 echo "개인 키, CSR, 인증서 생성 완료."
