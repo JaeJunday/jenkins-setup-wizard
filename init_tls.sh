17362094428dd42309a9383b3d4e009f8ac8cd11
jaejunday <wowns8114@gmail.com>
2024-07-28 21:48:11 +0900

    fix: no search file or directory

--- init_tls.sh
+++ init_tls.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+mkdir -p ./nginx/ssl
+
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./nginx/ssl/cert.key -out ./nginx/ssl/cert.pem -subj "/C=KR/ST=Seoul/L=Seoul/O=MyCompany/OU=MyDepartment/CN=www.mywebsite.com/emailAddress=admin@mywebsite.com"
 
 
